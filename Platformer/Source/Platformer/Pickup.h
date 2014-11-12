ebe0f07b7344ad6f7f8abb13b9b624a609dd3623
j-salem <john.d.salem@gmail.com>
2014-11-12 00:03:49 -0600

    Added Pickup Class

---
+++ Platformer/Source/Platformer/Pickup.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER_API APickup : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
