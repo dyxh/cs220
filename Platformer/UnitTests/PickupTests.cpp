38c87bfc253e9b26a0f375a99d54de4f4abd4959
M C <maxchen2412@gmail.com>
2014-11-18 19:36:33 -0600

    yes

--- Platformer/UnitTests/PickupTests.cpp
+++ Platformer/UnitTests/PickupTests.cpp
@@ -3,3 +3,20 @@
 #include "Pickup.h"
 #include "APaperPlatformerCharacter.h"
 
+class TEST_PICKUP: public ::testing::Test {
+protected:
+    APaperPlatformerCharacter character; // default values for all members
+
+};
+
+
+TEST_F(TEST_PICKUP, TEST_STAMINA){
+    
+    float beforeAttackStamina = character.Stamina;
+    character.PlayerInput(ETestInput::Attack_Pressed);
+    EXPECT_TRUE(character.BattleState == attackState);
+    
+    
+    
+    
+}
