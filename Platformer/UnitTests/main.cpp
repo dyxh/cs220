78afc173f824651be5c42d1f012e85911e238e5a
adamwyeth2 <adamwyeth@uchicago.edu>
2014-11-26 23:09:27 -0600

    main

---
+++ Platformer/UnitTests/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  UnitTests
+//
+//  Created by John Salem on 11/13/14.
+//  Copyright (c) 2014 EpicGames. All rights reserved.
+//
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
